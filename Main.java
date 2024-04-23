da7e1e0ad974ff1d57ec5d79b1ac2895dbd131b0
MeanMo <140993349+Yanndri@users.noreply.github.com>
2024-04-23 20:03:17 +0800

    Update Main.java
    Add Board indexes

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
 // Main Class File
+public class Main {
+	
+	public static void main(String[] args) {
+		int n=1;
+		for (int r=1; r<=10; r++){
+		    for(int c=1; c<=10; c++){
+		        if(r==1 || r==10)
+		        System.out.print("====");
+		        else if(c==1 || c==10)
+		        System.out.print(" || ");
+		        else
+		        System.out.printf(" %2d ",n++);
+		        }
+		        System.out.print("\n");
+		}
+	
+
+	}
+}
