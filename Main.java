ddc2f76eab887376727db2fd3cb228268ec0e3fe
MeanMo <140993349+Yanndri@users.noreply.github.com>
2024-04-23 21:11:41 +0800

    Update Main.java
    stored cells in a Matrix

--- Main.java
+++ Main.java
@@ -1,20 +1,37 @@
 // Main Class File
 public class Main {
-	
-	public static void main(String[] args) {
-		int n=1;
-		for (int r=1; r<=10; r++){
-		    for(int c=1; c<=10; c++){
-		        if(r==1 || r==10)
-		        System.out.print("====");
-		        else if(c==1 || c==10)
-		        System.out.print(" || ");
-		        else
-		        System.out.printf(" %2d ",n++);
-		        }
-		        System.out.print("\n");
-		}
-	
 
-	}
+    public static void main(String[] args) {
+        int cell = 0;
+        char[][] cells = new char[8][8];
+        String startPieces = "RNBQKBNRPPPPPPPP////////////////////////////////PPPPPPPPRNBQKBNR";
+
+        System.out.println("========================================");
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+
+                if (startPieces.charAt(cell) != '/')
+                    cells[row][col] = startPieces.charAt(cell);
+                else
+                    cells[row][col] = '-';
+                if ((cell + 1) % 8 == 1)
+                    System.out.print(" || ");
+                // System.out.printf(" %2c ", cells[row][col]);
+                System.out.printf(" %2d ", cell + 1);
+                if ((cell + 1) % 8 == 0)
+                    System.out.print(" || ");
+                cell++;
+            }
+            System.out.println();
+        }
+        System.out.println("========================================");
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++)
+                System.out.printf(" %2c ", cells[row][col]);
+            System.out.println();
+        }
+
+    }
+
 }
